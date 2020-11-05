ebd79c13f9575d8aff9de1e3f5f61038bab220ce
Aman Palariya <amanpalariya@gmail.com>
2020-11-05 19:30:51 +0530

    Fix problem with private variables

--- Code/gui/widgets/widget.py
+++ Code/gui/widgets/widget.py
@@ -1,17 +1,18 @@
 from ..utils import tk, Alignment
 
 class Widget():
-    __tk_object = None
+    tk_object = None
 
     def __init__(self, hidden=False):
         self.initially_hidden = hidden
+        self.hidden_status = True
 
-    def getNewTkObject(self, window, row, column, alignment):
-        raise Error('"getNewTkObject" function not implemented (contact Aman Palariya if you see this error)')
+    def initializeTkObject(self, window):
+        raise Error('"initializeTkObject" function not implemented (contact Aman Palariya if you see this error)')
 
     def makeTkObject(self, window, row, column, alignment):
-        if(not self.__tk_object):
-            self.__tk_object = self.getNewTkObject(window, row, column, alignment)
+        if(not self.tk_object):
+            self.initializeTkObject(window)
         self.row = row
         self.column = column
         self.sticky = Alignment.get_sticky_value_from_alignment(alignment)
@@ -22,25 +23,25 @@ class Widget():
         return self.hidden_status
 
     def hide(self):
-        if self.__tk_object:
-            self.__tk_object.grid_forget()
+        if self.tk_object:
+            self.tk_object.grid_forget()
         self.hidden_status = True
 
     def show(self):
-        if self.__tk_object:
-            self.__tk_object.grid(row=self.row, column=self.column, sticky=self.sticky)
+        if self.tk_object:
+            self.tk_object.grid(row=self.row, column=self.column, sticky=self.sticky)
             self.hidden_status = False
 
     def is_disabled(self):
-        return self.__tk_object['state'] == tk.DISABLED
+        return self.tk_object['state'] == tk.DISABLED
 
     def is_enabled(self):
         return not self.is_disabled()
 
     def disable(self):
-        if(self.__tk_object):
-            self.__tk_object['state'] = tk.DISABLED
+        if(self.tk_object):
+            self.tk_object['state'] = tk.DISABLED
 
     def enable(self):
-        if(self.__tk_object):
-            self.__tk_object['state'] = tk.NORMAL
+        if(self.tk_object):
+            self.tk_object['state'] = tk.NORMAL
