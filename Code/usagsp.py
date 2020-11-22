d0c716b1b0e998ef525cfadc768718e1014dbb53
Aman Palariya <55750085+amanpalariya@users.noreply.github.com>
2020-11-22 19:09:32 +0530

    Create usagsp.py

---
+++ Code/usagsp.py
@@ -0,0 +1,69 @@
+from synthnet import *
+from simulation import *
+from detcom import *
+import matplotlib.pyplot as plt
+import numpy as np
+from networkx.generators.community import LFR_benchmark_graph
+
+def af(G):
+    G = add_feature_vector(G, [('uniform', (0, 1))]*5)
+
+def av(G, l=0, u=1):
+    G = add_vulnerability_of_node(G, 'uniform', l, u)
+
+def aa(G):
+    G = add_age_to_edges(G)
+
+def show(G):
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+def get_angle(x, y):
+    return np.sum(x*y)/(np.linalg.norm(x)*np.linalg.norm(y))
+
+def get_commonness(G, u, v):
+    return get_angle(G.nodes[u]['feature'], G.nodes[v]['feature'])
+
+def get_empty_graph(n):
+    G = nx.Graph()
+    G.add_nodes_from(list(range(n)))
+    return G
+
+def commonness_between(G, group1, group2):
+    ar = []
+    for u in group1:
+        for v in group2:
+            ar.append(get_commonness(G, u, v))
+    return ar
+
+
+def analyse(G, clusters):
+    commonness = []
+    for i in range(len(clusters)):
+        ar = []
+        for j in range(len(clusters)):
+            if j < i:
+                ar.append(None)
+            else:
+                ar.append(commonness_between(G, clusters[i], clusters[j]))
+        commonness.append(ar)
+    return commonness
+
+def ppanalyse(y, func):
+    n = len(y)
+    for i in range(-1, n):
+        if i<0:
+            print(" "*5, end=' ')
+        else:
+            print(f"{i}".center(5), end=' ')
+    print()
+    for i in range(n):
+        for j in range(-1, n):
+            if j<0:
+                print(f"{i}".rjust(5), end=' ')
+            else:
+                if y[i][j]:
+                    print(f"{func(y[i][j])}     "[:5], end=' ')
+                else:
+                    print(" "*5, end=' ')
+        print()
